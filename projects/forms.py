294fbdcdab64f122cebd5be2b3ff14af8adf386e
Haruto Mori <baystarsman@outlook.jp>
2023-04-07 22:33:48 +0200

    build a function to add new project from the user

---
+++ projects/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Project
+
+# create a form based on the Project model in models.py
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = '__all__'
