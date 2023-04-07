dbcfac5aaf41481ea03fd4ddf4e4520c127d663a
Haruto Mori <baystarsman@outlook.jp>
2023-04-07 23:41:34 +0200

    create a funciton to update the project

--- projects/views.py
+++ projects/views.py
@@ -26,6 +26,20 @@ def create_project(request):
             form.save() # create an object
             return redirect('projects')  # redirect to the designated path
 
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)    # get the specific project with using pk
+    form = ProjectForm(instance=project)    # take the project as an instance, this will pre-fill in the form
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+
+        # check if the form is valid
+        if form.is_valid():
+            form.save() # create an object
+            return redirect('projects')  # redirect to the designated path
 
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
