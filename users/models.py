dc6e8d7346ae8c8e3573fe68c18d15ddc388c8e1
Haruto Mori <baystarsman@outlook.jp>
2023-04-10 21:23:20 +0200

    create profile models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    short_intro = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
+    social_github = models.CharField(max_length=200, blank=True, null=True)
+    social_twitter = models.CharField(max_length=200, blank=True, null=True)
+    social_linkedin = models.CharField(max_length=200, blank=True, null=True)
+    social_youtube = models.CharField(max_length=200, blank=True, null=True)
+    social_website = models.CharField(max_length=200, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)   # create a time stamp when the instance is created
+    # actually, django creates an id by default, but you shold use uuid in some cases
+    id = models.UUIDField(
+        default=uuid.uuid4, # encoding type
+        unique=True,    # no other same number
+        primary_key=True,   # tell djnago to use id as a primary key
+        editable=False  # protect editing id from users
+    )
+
+    def __str__(self):
+        return str(self.uesr.username)
