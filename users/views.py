db60bee5a2957ab631a937d9cafce57ef3db3597
Haruto Mori <baystarsman@outlook.jp>
2023-04-17 11:48:35 +0900

    add flash messages for login errors

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 
@@ -18,7 +19,7 @@ def login_user(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print('Username does not exist')
+            messages.error(request, 'Username does not exist')
         
         user = authenticate(request, username=username, password=password)
 
@@ -26,12 +27,13 @@ def login_user(request):
             login(request, user)
             return redirect('profiles')
         else:
-            print('Username or password is in correct')
+            messages.error(request,'Username or password is in correct')
 
     return render(request, 'users/login_register.html')
 
 def logout_user(request):
     logout(request)
+    messages.error(request, 'User was logged out!')
     return redirect('login')
 
 def profiles(request):
