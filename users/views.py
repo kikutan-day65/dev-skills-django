4f25b40a64db96cb05f92c24c257c94431faf821
Haruto Mori <baystarsman@outlook.jp>
2023-04-20 09:49:13 +0900

    make user profile be editable

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 # Create your views here.
 
@@ -91,3 +91,20 @@ def user_account(request):
         'projects': projects,
     }
     return render(request, 'users/account.html', context)
+
+@login_required(login_url='login')
+def edit_account(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid:
+            form.save()
+
+            return redirect('account')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/profile_form.html', context)
