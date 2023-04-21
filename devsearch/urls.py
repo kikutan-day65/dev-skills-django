204fa5a83e935c7e04cd9d1c28afd35cb22aaadc
Haruto Mori <baystarsman@outlook.jp>
2023-04-21 11:54:05 +0900

    fix the url slash problem

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -5,8 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('projects', include('projects.urls')),
+    path('admin/', admin.site.urls),
+    path('projects/', include('projects.urls')),
     path('', include('users.urls')),
 ]
 
