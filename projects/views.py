9b9f8cfb9240d111c07697ae4151b434f2bc0b92
Haruto Mori <baystarsman@outlook.jp>
2023-04-21 10:08:23 +0900

    biuld a search function in projects page

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Project
+from .models import Project, Tag
 from .forms import ProjectForm
+from .utils import search_project
 
 def projects(request):
-    projects = Project.objects.all()
+
+    projects, search_query =  search_project(request)
+    
     context = {
         'projects': projects,
+        'search_query': search_query
     }
     return render(request, 'projects/projects.html', context)
 
