fdc760d2faf0ffc5c00c26aea7de9608af493d97
Haruto Mori <baystarsman@outlook.jp>
2023-04-28 11:55:58 +0900

    add a decorator for permisson to access the user info in api

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import Project
@@ -17,6 +18,7 @@ def get_routes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_projects(reqeuset):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
